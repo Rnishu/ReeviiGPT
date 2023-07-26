d654f41a10b52c36570da2b3b220cdfbd53537a4
Rnishu <nishanthravichandran12@gmail.com>
2023-07-26 19:51:06 +0530

    Initial Commit:Adding my files

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-## Getting Started
-
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Class Node has value,grad,childrNodes,op.
+Neuron can be thought of as a vector, given an input the forward function will dot product the vector input and this neuron
+Layer is a group of neurons, that take a array of length nin, perform dot product of it with all the neurons, to output an array of the dot products of the neurons
--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
+//Class "Node" has value, operation, children(operands), and gradient
 class Node {
+    //attributes
     double value;
     char op;
     Node[] childrNodes=new Node[2];
     double grad;
+    //two constructors, one is the simple one that just defines value, the second is used for all different functions
     Node(double value){
         this.value=value;
     }
@@ -11,6 +14,7 @@ class Node {
         this.op=op;
         this.childrNodes=childrNodes;
     }
+    //Defines different functions, the most basic ones being add,mul,pow. Everything else is derived
     Node add(Node n2){
         Node[] children={this,n2};
         return (new Node(this.value+n2.value , '+',children ));
@@ -36,10 +40,12 @@ class Node {
     Node div(Node n2){
         return this.mul(n2.pow(-1));
     }
+    //only called on output, not recursive, sets gradient of output 1
     void backward(){
         this.grad=1;
         this.backpass();
     }
+    //Recursive function, decides gradient of children that is derived from gradient of parent and the operation done, and then calls the function on the children
     void backpass(){
         switch(this.op){
             case '+': this.childrNodes[0].grad+=this.grad;
@@ -57,6 +63,7 @@ class Node {
             break;
         }
     }
+    //creating static method for concatenation as it is used a lot
     static Node[] concat(Node[] arr1, Node[] arr2){
     Node[] sum=new Node[arr1.length+arr2.length];
     for(int i=0;i<arr1.length;i++)
@@ -66,13 +73,12 @@ class Node {
     return sum;
     }
 }
-
-
-
+//Class Neuron, each having weights and a bias, that input an array and output 1 number
 class Neuron{
+    //each neuron will have a array of weights and some bias to be added
     Node[] weights;
-
     Node bias;
+    //each neuron is initialized to have nin weights
     Neuron(int nin){
         this.weights=new Node[nin];
         for(int i=0;i<nin;i++){
@@ -88,7 +94,8 @@ class Neuron{
             sum[i]=sum[i-1].add(in[i].mul(this.weights[i]));
         return sum[in.length-2].add(in[in.length-1].mul(this.weights[in.length-1]));//sum[in.length-1];
         }
-    Node[] parameters(){
+    // Function to access all the parameters of a given layer
+        Node[] parameters(){
         Node[] para=new Node[this.weights.length+1];
         para[this.weights.length]=this.bias;
         for(int i=0;i<this.weights.length;i++){
@@ -97,8 +104,11 @@ class Neuron{
         return para;
     }
 }
+//Class Layer is a Group of neurons, taking some number of inputs and doing dot products with the vector assigned to each neuron, and giving an array of length of number of neurons in each layer
 class Layer{
+    //the group of neurons that each layer consists
     Neuron[] neurons;
+    //Constructor creates nout neurons each having nin weights
     Layer(int nin, int nout){
         this.neurons=new Neuron[nout];
         for(int i=0;i<nout;i++){
@@ -112,6 +122,7 @@ class Layer{
             output[i]=this.neurons[i].forward(in);
         return output;
     }
+    //return array of parameters
     Node[] parameters(){
         /*Node[] para=new Node[this.neurons.length*(this.neurons[0].weights.length+1)];
         Node[] temp=new Node[this.neurons[0].weights.length+1];
@@ -130,17 +141,9 @@ class Layer{
 }
 public class App{
     public static void main(String[] args){
-        /*Node n1=new Node(55);
-        Node n2=new Node(12);
-        Node n3=new Node(23);
-        Node n5=n1.sub(n2);
-        Node n6=n3.pow(4);
-        Node n7=n5.add(n6);*/
         Node[] arr={new Node(4), new Node(2),new Node(17)};
         Layer n1=new Layer(3,4);
-        //Layer n2=new Layer(4,1);
         Node[] out1=n1.forward(arr);
-        //Node[] out2=n2.forward(out1);
         System.out.println("output="+out1[0].value);
         out1[0].backward();
         Node[] parameters=n1.parameters();
